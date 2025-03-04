cca6b98387d0ec0362aeba6af7cd6d64c1b24225
pasek <semesterempat92@gmail.com>
2025-03-04 21:51:19 +0800

    Keterangan commit

---
+++ Pertemuan 1/src/pertemuan/pkg1/Pertemuan1.java
@@ -0,0 +1,17 @@
+
+package pertemuan.pkg 01;
+
+/**
+ *
+ * @author Pasek
+ */
+public class Pertemuan 01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Belajar Bahasa Java");
+    }
+    
+}
---
+++ Pertemuan1/src/pertemuan1/Pertemuan1.java
@@ -0,0 +1,17 @@
+
+package pertemuan1;
+
+/**
+ *
+ * @author Pasek
+ */
+public class Pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Belajar Bahasa Java");
+    }
+    
+}
